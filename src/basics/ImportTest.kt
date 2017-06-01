15302cf4df03cfdb21e03d64f36228308da6ce37
Divya Gupta <divagupta2392@gmail.com>
2017-06-01 16:54:56 +0530

    packages and imports in kotlin

---
+++ src/basics/ImportTest.kt
@@ -0,0 +1,13 @@
+package basics
+
+import basics.utils.SomeUtilsClass
+
+/**
+ * Created by divya on 01/06/17.
+ */
+
+fun main(args: Array<String>) {
+    println("Main")
+
+    SomeUtilsClass("Utils class")
+}
---
+++ src/basics/utils/SomeUtilsClass.kt
@@ -0,0 +1,9 @@
+package basics.utils
+
+/**
+ * Created by divya on 01/06/17.
+ */
+fun SomeUtilsClass(name: String)
+{
+
+}
